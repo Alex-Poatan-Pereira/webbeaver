561fcf08ad05315dc12f33fb19b4d202ab30dd06
Alex <hwaeeting0104@gmail.com>
2025-03-31 16:31:44 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
-<!-- index.php -->
-<?php
-session_start(); // 세션 시작
-
-// 로그인된 사용자 정보 확인
-if (isset($_SESSION['user_id'])) {
-    echo "환영합니다, " . $_SESSION['username'] . "님!";
-    // 추가적인 로그인 후 콘텐츠 표시
-    echo "<br><a href='logout.php'>로그아웃</a>";
-} else {
-    echo "로그인되지 않았습니다.";
-    // 로그인 페이지로 리디렉션
-    header("Location: backend/login.php");
-    exit();
-}
-?>
+<!DOCTYPE html>
+<?php session_start(); ?>
+<html>
+        <head>
+                <meta charset="utf-8" />
+                <title>PHP Session Login Test</title>
+                <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+                <h1>Webbeaver</h1>
+                <?php
+                        if(!isset($_SESSION['user_id']) || !isset($_SESSION['user_name'])) {
+                                echo "<p>로그인을 해 주세요. <a href=\"./users/login.php\">[로그인]</a></p>";
+                        } else {
+                                $user_id = $_SESSION['user_id'];
+                                $user_name = $_SESSION['user_name'];
+                                echo "<p><strong>$user_name</strong>($user_id)님 환영합니다.";
+                                echo "<a href=\"./users/logout.php\">[로그아웃]</a></p>";
+                        }
+                ?>
+                
+                
+        </body>
+</html>
